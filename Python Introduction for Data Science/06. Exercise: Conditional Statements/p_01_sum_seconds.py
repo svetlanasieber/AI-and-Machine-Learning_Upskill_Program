e32ced62d60565b5bf8efcdcfa0763a63e6e3fb3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-06 00:08:37 +0100

    Update p_01_sum_seconds.py

--- Python Introduction for Data Science/06. Exercise: Conditional Statements/p_01_sum_seconds.py
+++ Python Introduction for Data Science/06. Exercise: Conditional Statements/p_01_sum_seconds.py
@@ -4,7 +4,7 @@ time_first = int(input())
 time_second = int(input())
 time_third = int(input())
 
-#35 45 44 => 124 // 60 = 2 minuten -> ostatuk 4 secunden
+
 
 total_time = time_first + time_second + time_third
 
@@ -13,10 +13,7 @@ seconds = total_time % 60
 
 minutes = math.floor(minutes)
 
-# Шаблон
-# print(f"{minutes}:{seconds:02}")
 
-# ако секундите са едноцифрено число, да принтира с 0 отпред
 if seconds < 10:
     print(f"{minutes}:0{seconds}")
 else:
