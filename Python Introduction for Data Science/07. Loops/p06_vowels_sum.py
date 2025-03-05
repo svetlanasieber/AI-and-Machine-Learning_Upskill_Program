52951f326141c4e5b1d8210287277b45d94a285c
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-06 00:17:55 +0100

    Update p06_vowels_sum.py

--- Python Introduction for Data Science/07. Loops/p06_vowels_sum.py
+++ Python Introduction for Data Science/07. Loops/p06_vowels_sum.py
@@ -1,9 +1,3 @@
-# 6.	Сумиране на гласните букви
-# Да се напише програма, която чете текст (стринг), въведен от потребителя,
-# и изчислява и отпечатва сумата от стойностите на гласните букви според таблицата по-долу:
-
-# sum_of_points = sum_of_points +5 e analogichno na sum_of_points +=5 - Ivan Shopov
-
 some_string = input()
 sum_of_points = 0
 for character in some_string:
