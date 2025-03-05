6ab98edc56b83fc41c02c932b5fcd2cb8bf8ff32
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-06 00:16:32 +0100

    Update 06_max_number.py

--- Python Introduction for Data Science/07. Loops/06_max_number.py
+++ Python Introduction for Data Science/07. Loops/06_max_number.py
@@ -9,15 +9,4 @@ while command != "Stop":
     command = input()
 print(max_number)
 
-# 2 variant ohne sys max size
-#max_number = 0
-#command = input()
-#if command != "Stop":
-#    max_number = int(command)
-#command = input()
-#while command != "Stop":
-#    current_number = int(command)
-#    if current_number > max_number:
-#        max_number = current_number
-#        command = input()
-#print(max_number)
+
